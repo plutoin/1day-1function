a07176e8c7e897feb52d0581a86afeae0143a9eb
plutoin <uleage@gmail.com>
2022-06-07 17:29:57 +0900

    05.31

---
+++ 1day-1code/10_8의_개수_세기.js
@@ -0,0 +1,5 @@
+// 05.31
+
+(Array(100).fill(1).map((value, index) => value + index) + '').split('8').length - 1;
+
+(Array(1000).fill(1).map((value, index) => value + index) + '').split('8').length - 1;
